8f705f7f52925cdc0475aaad514f402501a9a8cc
atanasova07 <adriana0702@abv.bg>
2023-02-14 12:58:18 +0200

    Game over functionality

--- scoreboard.py
+++ scoreboard.py
@@ -2,6 +2,7 @@ from turtle import Turtle
 ALIGNMENT = "center"
 FONT = ("Ariel", 20, "normal")
 
+
 class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
@@ -14,6 +15,10 @@ class Scoreboard(Turtle):
 
     def update_scoreboard(self):
         self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write("Game over", align=ALIGNMENT, font=FONT)
 
     def increase_score(self):
         self.score += 1
