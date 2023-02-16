9193658fdbb582013baef880b5d3ab30ddd18c7f
atanasova07 <adriana0702@abv.bg>
2023-02-16 23:31:06 +0200

    Added different shapes

--- food.py
+++ food.py
@@ -1,18 +1,50 @@
 from turtle import Turtle
 import random
 
+SHAPES = ("circle", "turtle", "triangle", "square")
+
+
 class Food(Turtle):
 
     def __init__(self):
         super().__init__()
-        self.shape("circle")
+        self.choose_shape()
         self.penup()
+        self.refresh()
+
+    def choose_shape(self):
+        self.shape(random.choice(SHAPES))
+        if self.shape() == "turtle":
+            self.turtle_shape()
+        elif self.shape() == "circle":
+            self.circle_shape()
+        elif self.shape() == "triangle":
+            self.triangle_shape()
+        else:
+            self.square_shape()
+
+    def turtle_shape(self):
+        self.shapesize(stretch_len=0.8, stretch_wid=0.8)
+        self.color("green")
+        self.speed("fastest")
+
+    def circle_shape(self):
         self.shapesize(stretch_len=0.5, stretch_wid=0.5)
         self.color("yellow")
         self.speed("fastest")
-        self.refresh()
+
+    def triangle_shape(self):
+        self.shapesize(stretch_len=0.5, stretch_wid=0.5)
+        self.color("red")
+        self.speed("fastest")
+
+    def square_shape(self):
+        self.shapesize(stretch_len=0.5, stretch_wid=0.5)
+        self.color("orange")
+        self.speed("fastest")
 
     def refresh(self):
         random_x = random.randint(-280, 280)
-        random_y = random.randint(-280, 280)
+        random_y = random.randint(-280, 260)
+        self.choose_shape()
         self.goto(random_x, random_y)
